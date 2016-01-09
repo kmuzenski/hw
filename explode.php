cc32831a0ed534a128632918038264eab5bda244
kmuzenski <@kathrynmuzenski@yahoo.com>
2016-01-09 16:12:36 +0000

    explode.php

--- explode.php
+++ explode.php
@@ -1,7 +1,25 @@
 <?php
+	
 
-	$myString = "I love Kitties";
-	$myArray = explode(" ", $myString);
-	print_r ($myArray);
+	function explosion($delim, $sentence) {
+	$space ="";
+	$returnarray = array();
+ 
+	for($i =0; $i < strlen($sentence); $i++) {
+	if($sentence[$i] == " ") {
+	array_push($returnarray,$space);
+	$space ="";
+}
+    else {
+	$space =$space.$sentence[$i];
+	}
+}
+
+array_push($returnarray, $space);
+return $returnarray;
+
+}
+
+ print_r (explosion(" ","I love kitties."));
 
 ?>
