72b091e912649f53bf8294cf52fb20f42705b51c
kmuzenski <@kathrynmuzenski@yahoo.com>
2016-01-15 00:18:02 +0000

    revision.php

--- revision.php
+++ revision.php
@@ -5,112 +5,121 @@ class Associate {
 
 //constructor applies to all new associate
 public function __construct($firstName, $memberID)
-{
-$this->firstName = $firstName;
-$this->memberID = $memberID;
+	{
+	$this->firstName = $firstName;
+	$this->memberID = $memberID;
 
-}
+	}
 
 
-function average($values) {
-$elementSum = 0;
+public function average($values) {
+		$elementSum = 0;
 
-for ($i = 0; $i <count($values); $i++)
-{
 
-$elementSum = $elementSum + $values[$i];
-}
 
-print_r ($elementSum);
-if(count($values) == 0)
 
-{
-return 0;
-}
-else {
-return $elementSum / count($values);
-}
-}
+		for ($i = 0; $i <count($values); $i++)
+		{
+		$elementSum = $elementSum + $values[$i];
+		}
 
-function getRank() {
- $scores = array("Darach" => array(90,80,89),
+		if(count($values) == 0)
+
+		{
+		return 0;
+		}
+		else
+		{
+		return $elementSum / count($values);
+		}
+		
+	}
+
+
+
+public function getRank() {
+	 $scores = array("Darach" => array(60,62,66),
                 "Debbie" => array(49,39,29),
                 "Bird" => array(97,90,92)
                 );
-print_r($scores);
-foreach($scores as $key => $score)
-{
-echo $key;
-print_r ($score);
-$avgScore = $this->average($score);
 
-if($avgScore < 50) 
-{
-$retVal = "Third  Tier. \n";
-}
-else if ($avgScore < 70)
-{
-echo "Second Tier. \n";
-}
-else {
-$retVal = "Top Tier. \n";
-}
-return $retVal;
-}
+	foreach($scores as $key => $score)
+		{
+		if($key != $this->firstName)
+		{
+		continue;
+		}	
 
-foreach(array_keys($scores) as $name)
-{
-$avgScore = average($Ranks[$ranked]);
-$skill = getRank($avgScore);
+		$avgScore = $this->average($score);
+		$retVal = "Rank is Top Tier. \n";
+			
+		if($avgScore < 50) 
+		{
+			$retVal = "Rank is Third  Tier. \n";
+		}
 
-echo "$name is currently ranked $skill with an average of $avgRank. \n";
-}
-}
+		else if ($avgScore < 70)
+		{
+			$retVal= "Rank is Second Tier. \n";
+		}
+		}
 
-public function listName() //calls to constructor function
-{
-echo $this->firstName."\n";
-}
+		return $retVal;
+	}
+	
 
-public function listID()
-{
-echo $this->memberID."\n";
-}
+	public function listName() //calls to constructor function
+	{
+		echo $this->firstName."\n";
+	}
+
+	public function listID()
+	{
+		echo $this->memberID."\n";
+	}
+
 }
 
 
 
 class assistantM extends Associate { //inherites from parent class
 
-function salesReport() 
-{
-$hourly=0;
-while ($hourly <= 10)
-{
-$hourly +=1;
+	function salesReport() 
+		{
+		$hourly=0;
+		while ($hourly <= 10)
+		{
+			$hourly +=1;
 
-}
-echo "Sales Report is $hourly \n";
-}
+		}
+		echo "Sales Report is $hourly \n";
+		}
 
-}
+	}
 
 
+
+//calls to functions or creates new object 
 $Debbie = new assistantM("Debbie","018");
 $Debbie->listName();
+$Debbie->listID();
+
 print_r($Debbie->getRank());
+
+print_r ("\n");
 $Darach = new Associate("Darach","000");
- //creates new object for the class
 $Darach->listName();
 $Darach->listID();
- //calls to function listName
 print_r($Darach->getRank());
+
+print_r("\n");
 $Bird = new Associate("Bird","001");
 $Bird->listName();
 $Bird->listID();
+print_r($Bird->getRank());
 
-
+print_r("\n");
 print_r($Debbie->salesReport());
 
-
+ 
 ?>
